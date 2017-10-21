3ab6f27b8e57ce72d2ab8b12a7a073650376a4b4
sheqi0399 <sheqi1991@gmail.com>
2017-10-21 15:23:24 -0400

    VAE description

--- VAE.py
+++ VAE.py
@@ -4,6 +4,7 @@ References
 ----------
 http://edwardlib.org/tutorials/decoder
 http://edwardlib.org/tutorials/inference-networks
+simple demo of using vae for MNIST
 """
 from __future__ import absolute_import
 from __future__ import division
