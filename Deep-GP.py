800a796d5bf440c35013c9e78f3d3848edd7bbd4
sheqi <sheqi1991@gmail.com>
2017-11-19 13:58:31 -0500

    deep-GP

--- Deep-GP.py
+++ Deep-GP.py
@@ -17,12 +17,23 @@ N = data.shape[0]
 D = 2 # number of features
 K = 2 # number of latent dimensions
 
+H1 = 2
+H2 = 2
+
 # Model: deep/shallow GP (generative model)
 X = Normal(loc=tf.zeros([N,K]),scale=tf.ones([N,K]))
-
 Kernal = rbf(X)+ tf.eye(N) * 1e-6
-cholesky = tf.tile(tf.reshape(tf.cholesky(Kernal),[1,N,N]),[D,1,1])
-Y = MultivariateNormalTriL(loc=tf.zeros([D,N]), scale_tril=cholesky)
+cholesky = tf.tile(tf.reshape(tf.cholesky(Kernal),[1,N,N]),[H1,1,1])
+
+h1 = MultivariateNormalTriL(loc=tf.zeros([H1,N]), scale_tril=cholesky)
+Kernal1 = rbf(tf.transpose(h1))+ tf.eye(N) * 1e-6
+cholesky1 = tf.tile(tf.reshape(tf.cholesky(Kernal1),[1,N,N]),[H2,1,1])
+
+h2 = MultivariateNormalTriL(loc=tf.zeros([H2,N]), scale_tril=cholesky1)
+Kernal2 = rbf(tf.transpose(h2))+ tf.eye(N) * 1e-6
+cholesky2 = tf.tile(tf.reshape(tf.cholesky(Kernal2),[1,N,N]),[D,1,1])
+
+Y = MultivariateNormalTriL(loc=tf.zeros([D,N]), scale_tril=cholesky2)
 
 # Inference (recongnition model)
 qX = Normal(loc=tf.Variable(tf.random_normal([N,K])),
